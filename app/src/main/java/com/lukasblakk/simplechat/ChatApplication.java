9474ce922a23dc1cab5192a3007bd2f6ebf1d120
Stephan Duroseau <sduroseau1@pride.hofstra.edu>
2018-12-15 01:00:47 -0500

    commit

---
+++ app/src/main/java/com/lukasblakk/simplechat/ChatApplication.java
@@ -0,0 +1,25 @@
+package com.lukasblakk.simplechat;
+
+import android.app.Application;
+
+import com.lukasblakk.simplechat.models.Message;
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.interceptors.ParseLogInterceptor;
+
+
+
+public class ChatApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ParseObject.registerSubclass(Message.class);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+            .applicationId("93bd0b59a90d46b1999e484431b83f41")
+                .addNetworkInterceptor(new ParseLogInterceptor())
+                .server("https://simplechatclient.herokuapp.com/parse/").build());
+
+    }
+}
